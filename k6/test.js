98f2da5f980660c7935cf26c32770539d3179819
David Wiles <dwiles@axon.com>
2023-06-20 10:17:46 -0500

    Update test and remove keys.txt

--- k6/test.js
+++ k6/test.js
@@ -1,10 +1,14 @@
 import http from "k6/http";
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import { SharedArray } from 'k6/data';
 
-// The targeted percentage of commands which should be writes in the tested workload
-const writePct = 0.2
+// The targeted percentage of commands which should be writes
+const writePct = __ENV.WRITE_PERCENTAGE || 0.2
 
-const rate = 2000;
+// Rate of reads throughout the test. The actual rate of HTTP requests may be greater
+// since a write would be included in the same test
+const rate = __ENV.RATE || 300;
+
 const keys = new SharedArray('keys', function() {
   const data = open('keys.txt');
   return data.split("\n");
@@ -21,17 +25,25 @@ export const options = {
       rate: rate,
       timeUnit: '1s',
       preAllocatedVUs: 100,
-      maxVUs: 500
+      maxVUs: 1000
     }
   }
 };
 
 export default function () {
   const now = new Date();
-  const key = keys[now.getMilliseconds() % keys.length];
+
+  // Random keys are used for each VU to ensure that key ordering does not
+  // affect which Redis host is used for a key
+  const key = keys[randomIntBetween(0, keys.length-1)];
   const domain = "localhost:9000";
 
+  // Get the key from cache
   const getResponse = http.get(`http://${domain}/get/${key}`);
   const v = getResponse.json('value');
-  if (!v || now - Date.parse(v) > ms) http.post(`http://${domain}/set/${key}/${now}`);
+
+  // If the key is not found, or the cached value is a date greater than our
+  // artificial TTL, we should overwrite the value with the current time
+  if (!v || now - Date.parse(v) > ms)
+    http.post(`http://${domain}/set/${key}/${now}`);
 }
